5c88ec6c197171275fc5136646c1f0db91dfee0a
lizd <lizd@ios.ac.cn>
2024-11-22 01:03:30 +0800

    修改server 但目前还是没法应对大点云

--- socket_server_para.py
+++ socket_server_para.py
@@ -4,6 +4,7 @@ import numpy as np
 import json
 import torch
 import util
+import threading
 torch.manual_seed(1)
 
 # 设置服务器的IP和端口
@@ -11,10 +12,15 @@ HOST = '0.0.0.0'  # 监听所有IP地址
 PORT = 12344     # 监听的端口号
 REQUEST_BUFFER_SIZE = 1000 # 接收缓冲区大小，单位为字节
 max_thread = 50 # 同时处理的最大线程数
-        
+
+max_pts = 30000 * 30000 # 同时处理的最大点数
+res_pts = max_pts # 当前剩余点数
+pts_mtx = threading.Lock()     
+    
 device = torch.device(torch.cuda.current_device() if torch.cuda.is_available() else torch.device('cpu'))
 
 def log_msg(msg,mode='error'):
+    current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     if mode == 'error':
         print("\033[1;31m" + msg + "\033[0m")
     elif mode == 'warning':
@@ -24,8 +30,42 @@ def log_msg(msg,mode='error'):
         
     log_path = mode + ".log"
     with open(log_path, 'a') as f:
-        f.write(msg + '\n')
+        f.write(current_time + ' ' + msg + '\n')
     return
+
+# 申请处理点资源，如果点数超过最大值，则等待
+def apply_pts(len_pts):
+    sum_pts = len_pts * len_pts
+    global res_pts
+    global pts_mtx
+    global max_pts
+    if sum_pts > max_pts:
+        msg = f"Too many points to process at once. {sum_pts} points requested, but only {max_pts} points allowed."
+        log_msg(msg,mode='error')
+        print(msg)
+        assert False
+
+    pts_mtx.acquire()
+    flag = True
+    while sum_pts > res_pts:
+        pts_mtx.release()
+        if flag:
+            print(f"Waiting for points. Apllying {sum_pts} points, but only {res_pts} points left.")
+            flag = False
+        time.sleep(20)
+        pts_mtx.acquire()
+    res_pts -= sum_pts
+    pts_mtx.release()
+    return    
+
+def release_pts(len_pts):
+    global res_pts
+    global pts_mtx
+    pts_mtx.acquire()
+    res_pts += len_pts * len_pts
+    pts_mtx.release()
+    return
+
 
 def tree_xie_propagation(xyz_data,config):
     input_pc = util.npxyz2tensor(xyz_data).to(device)
@@ -117,7 +157,7 @@ def handle_client(conn, addr):
                 assert False
             # 假设接收到的数据是二进制形式的XYZ浮点数数组
             xyz_data = np.frombuffer(data, dtype=np.float64).reshape(-1, 3)
-            
+            apply_pts(len(xyz_data))
             # 计算法向量
             if req['function_name'] == 'simple_estimate':
                 transformed_pc = simple_estimate(xyz_data, req['function_config'])
@@ -141,6 +181,7 @@ def handle_client(conn, addr):
 
             # 返回结果
             conn.sendall(result.astype(np.float64).tobytes())
+            release_pts(len(xyz_data))
         except Exception as e:
             print(f"Error: {e}")
             log_msg(f"Error: {e}",mode='error')
